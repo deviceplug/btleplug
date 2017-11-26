9ff9327f01cdff00e73fa8d23e4a18494377f89e
Micah Wylde <micah@micahw.com>
2017-11-26 12:38:21 -0800

    Working on higher-level API for adapters

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,32 @@ use std::mem;
 use std::ffi::{CString, CStr};
 use nix::sys::ioctl;
 use nix::Errno;
+use std::collections::HashSet;
+use std::fmt;
+use std::fmt::{Display, Debug, Formatter};
 
 #[derive(Copy)]
-#[derive(Debug)]
 #[repr(C)]
 pub struct BDAddr {
-    pub b : [ u8 ; 6usize ]
+    pub address: [ u8 ; 6usize ]
 }
 
 impl Clone for BDAddr {
     fn clone(&self) -> Self { *self }
+}
+
+impl Display for BDAddr {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let a = self.address;
+        write!(f, "{:X}:{:X}:{:X}:{:X}:{:X}:{:X}",
+                a[5], a[4], a[3], a[2], a[1], a[0])
+    }
+}
+
+impl Debug for BDAddr {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        (self as &Display).fmt(f)
+    }
 }
 
 #[derive(Copy)]
@@ -65,7 +81,6 @@ impl Clone for HCIDevListReq {
     fn clone(&self) -> Self { *self }
 }
 
-
 #[derive(Copy)]
 #[repr(C)]
 pub struct HCIDevStats {
@@ -83,6 +98,23 @@ pub struct HCIDevStats {
 
 impl Clone for HCIDevStats{
     fn clone(&self) -> Self { *self }
+}
+
+impl HCIDevStats {
+    fn default() -> HCIDevStats {
+        HCIDevStats {
+            err_rx: 0u32,
+            err_tx: 0u32,
+            cmd_tx: 0u32,
+            evt_rx: 0u32,
+            acl_tx: 0u32,
+            acl_rx: 0u32,
+            sco_tx: 0u32,
+            sco_rx: 0u32,
+            byte_rx: 0u32,
+            byte_tx: 0u32
+        }
+    }
 }
 
 #[derive(Copy)]
@@ -108,6 +140,86 @@ impl Clone for HCIDevInfo {
     fn clone(&self) -> Self { *self }
 }
 
+impl HCIDevInfo {
+    fn default() -> HCIDevInfo {
+        HCIDevInfo {
+            dev_id: 0,
+            name: [0i8; 8],
+            bdaddr: BDAddr { address: [0u8; 6] },
+            flags: 0u32,
+            type_: 0u8,
+            features: [0u8; 8],
+            pkt_type: 0u32,
+            link_policy: 0u32,
+            link_mode: 0u32,
+            acl_mtu: 0u16,
+            acl_pkts: 0u16,
+            sco_mtu: 0u16,
+            sco_pkts: 0u16,
+            stat: HCIDevStats::default()
+        }
+    }
+}
+
+#[derive(Debug)]
+enum AdapterType {
+    BrEdr,
+    Amp,
+    Unknown
+}
+
+impl AdapterType {
+    fn parse(typ: u8) -> AdapterType {
+        match typ {
+            0 => AdapterType::BrEdr,
+            1 => AdapterType::Amp,
+            _ => AdapterType::Unknown,
+        }
+    }
+}
+
+#[derive(Hash, Eq, PartialEq, Debug, Copy, Clone)]
+enum AdapterState {
+    Up, Init, Running, Raw, PScan, IScan, Inquiry, Auth, Encrypt
+}
+
+impl AdapterState {
+    fn parse(flags: u32) -> HashSet<AdapterState> {
+        use AdapterState::*;
+
+        let states = [Up, Init, Running, Raw, PScan, IScan, Inquiry, Auth, Encrypt];
+
+        let mut set = HashSet::new();
+        for (i, f) in states.iter().enumerate() {
+            if flags & (1 << (i & 31)) != 0 {
+                set.insert(f.clone());
+            }
+        }
+
+        set
+    }
+}
+
+#[derive(Debug)]
+struct Adapter {
+    name: String,
+    dev_id: i32,
+    bdaddr: BDAddr,
+    typ: AdapterType,
+    states: HashSet<AdapterState>,
+}
+
+impl Adapter {
+    fn from_device_info(di: &HCIDevInfo) -> Adapter {
+        Adapter {
+            name: String::from(unsafe { CStr::from_ptr(di.name.as_ptr()).to_str().unwrap() }),
+            dev_id: 0,
+            bdaddr: di.bdaddr,
+            typ: AdapterType::parse((di.type_ & 0x30) >> 4),
+            states: AdapterState::parse(di.flags),
+        }
+    }
+}
 
 #[derive(Copy)]
 #[repr(C)]
@@ -458,11 +570,6 @@ unsafe fn reset(dev_id: i32) {
     hci_dev_up(ctl, dev_id).unwrap();
 }
 
-struct Adapter {
-    dev_id: i32,
-    name: String
-}
-
 fn handle_error(v: i32) -> nix::Result<i32> {
     if v < 0 {
         Err(nix::Error::Sys(Errno::last()))
@@ -476,89 +583,48 @@ fn get_control_socket() -> nix::Result<i32> {
 }
 
 // #define HCIGETDEVLIST	_IOR('H', 210, int)
-ioctl!(read hci_get_dev_list with b'H', 210; i32);
-// #define HCIGETDEVINFO	_IOR('H', 211, int)
-ioctl!(read hci_get_dev_info with b'H', 211; i32);
-
-
 static HCI_GET_DEV_LIST_MAGIC: usize = (2u32 << 0i32 + 8i32 + 8i32 + 14i32 |
     (b'H' as (i32) << 0i32 + 8i32) as (u32) | (210i32 << 0i32) as (u32)) as
     (usize) | 4 /* (sizeof(i32)) */ << 0i32 + 8i32 + 8i32;
 
+// #define HCIGETDEVINFO	_IOR('H', 211, int)
 static HCI_GET_DEV_MAGIC: usize = (2u32 << 0i32 + 8i32 + 8i32 + 14i32 |
     (b'H' as (i32) << 0i32 + 8i32) as (u32) | (211i32 << 0i32) as (u32)) as (usize) |
     4 /* (sizeof(i32)) */ << 0i32 + 8i32 + 8i32;
 
 fn get_adapters() -> nix::Result<Vec<Adapter>> {
-    let result: Vec<Adapter> = vec![];
+    let mut result: Vec<Adapter> = vec![];
 
     let ctl = get_control_socket()?;
 
     println!("ctl = {}", ctl);
 
+    let mut buf = vec![0u8; 16usize * mem::size_of::<HCIDevReq>() + mem::size_of::<u16>()];
+    let dl: *mut HCIDevListReq = buf.as_mut_ptr() as (*mut HCIDevListReq);
+    let dr: *mut HCIDevReq;
+
     unsafe {
-        let mut dl: *mut HCIDevListReq;
-        let mut dr: *mut HCIDevReq;
-
-
-
-        dl = libc::malloc(
-            16usize.wrapping_mul(
-                ::std::mem::size_of::<HCIDevReq>()
-            ).wrapping_add(
-                ::std::mem::size_of::<u16>()
-            )) as (*mut HCIDevListReq);
-
         (*dl).dev_num = 16u16;
         dr = (*dl).dev_reqs.as_mut_ptr();
 
-        if dl.is_null() {
-            panic!("Failed to allocate memory");
-        }
         handle_error(libc::ioctl(ctl, HCI_GET_DEV_LIST_MAGIC as libc::c_ulong, dl as (*mut c_void)))?;
 
-        println!("size {}", (*dl).dev_num);
-
-        for i in 0..(*dl).dev_num {
-            let mut di = HCIDevInfo {
-                dev_id: (*dr.offset(i as isize)).dev_id,
-                name: [0i8; 8],
-                bdaddr: BDAddr { b: [0u8; 6] },
-                flags: 0u32,
-                type_: 0u8,
-                features: [0u8; 8],
-                pkt_type: 0u32,
-                link_policy: 0u32,
-                link_mode: 0u32,
-                acl_mtu: 0u16,
-                acl_pkts: 0u16,
-                sco_mtu: 0u16,
-                sco_pkts: 0u16,
-                stat: HCIDevStats {
-                    err_rx: 0u32,
-                    err_tx: 0u32,
-                    cmd_tx: 0u32,
-                    evt_rx: 0u32,
-                    acl_tx: 0u32,
-                    acl_rx: 0u32,
-                    sco_tx: 0u32,
-                    sco_rx: 0u32,
-                    byte_rx: 0u32,
-                    byte_tx: 0u32
-                }};
+         for i in 0..(*dl).dev_num {
+            let mut di = HCIDevInfo::default();
+            di.dev_id = (*dr.offset(i as isize)).dev_id;
 
             handle_error(libc::ioctl(ctl, HCI_GET_DEV_MAGIC as libc::c_ulong,
                                      &mut di as (*mut HCIDevInfo) as (*mut c_void)))?;
 
-            println!("{:?}", CStr::from_ptr(di.name.as_ptr()));
+            result.push(Adapter::from_device_info(&di));
         }
-    };
+    }
 
     Ok(result)
 }
 
 fn main() {
-    get_adapters().unwrap();
+    println!("adapters: {:#?}", get_adapters().unwrap());
 
 //    unsafe {
 //

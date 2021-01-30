8adaeff6643c55d5be9d50730ec2ce6b80a5df9a
Andrew Walbran <qwandor@google.com>
2021-01-30 18:33:09 +0000

    Implement async API on Linux using bluez-async.

---
+++ src/bluez_async/manager.rs
@@ -0,0 +1,24 @@
+use bluez_async::BluetoothSession;
+
+use super::adapter::Adapter;
+use crate::Result;
+
+#[derive(Clone, Debug)]
+pub struct Manager {
+    session: BluetoothSession,
+}
+
+impl Manager {
+    pub async fn new() -> Result<Self> {
+        let (_, session) = BluetoothSession::new().await?;
+        Ok(Self { session })
+    }
+
+    pub async fn adapters(&self) -> Result<Vec<Adapter>> {
+        let adapters = self.session.get_adapters().await?;
+        Ok(adapters
+            .into_iter()
+            .map(|adapter| Adapter::new(self.session.clone(), adapter))
+            .collect())
+    }
+}

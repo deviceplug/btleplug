2d613b4012b75257ea37b654502025607361c3a9
Andrew Walbran <qwandor@google.com>
2021-02-12 22:43:57 +0000

    Remove sync channels from AdapterManager.

--- src/api/adapter_manager.rs
+++ src/api/adapter_manager.rs
@@ -16,7 +16,6 @@ use dashmap::{mapref::one::RefMut, DashMap};
 use futures::channel::mpsc::{self, UnboundedSender};
 use futures::stream::Stream;
 use std::pin::Pin;
-use std::sync::mpsc::{channel, Receiver, Sender};
 use std::sync::{Arc, Mutex};
 
 #[derive(Clone, Debug)]
@@ -25,35 +24,14 @@ where
     PeripheralType: Peripheral,
 {
     peripherals: Arc<DashMap<BDAddr, PeripheralType>>,
-
-    // Sender is never handled mutably, but mpsc's Sender is Send only, not Sync,
-    // so we can't just wrap it in an Arc to pass it around as part of the adapter
-    // struct. I also don't want to use crossbeam channels, to avoid type leakage.
-    //
-    // This will be fixed when we go async in 1.0 and can use stream traits. For
-    // now, we deal with the lock timing.
-    event_sender: Arc<Mutex<Sender<CentralEvent>>>,
-
-    // Normally we'd just return the event receiver when an adapter is created.
-    // However, since adapters are cloned and retrieved via lists, this is really
-    // hard without changing the fundamentals of the API (which I want to do at
-    // some point, but not now). Storing an option here means that we'll only ever
-    // have one event receiver (as mpsc isn't clonable, which is what we want on
-    // the receiver side anyways), but means we also don't have to deal with the
-    // adapter API yet.
-    event_receiver: Arc<Mutex<Option<Receiver<CentralEvent>>>>,
-
     async_senders: Arc<Mutex<Vec<UnboundedSender<CentralEvent>>>>,
 }
 
 impl<PeripheralType: Peripheral + 'static> Default for AdapterManager<PeripheralType> {
     fn default() -> Self {
         let peripherals = Arc::new(DashMap::new());
-        let (event_sender, event_receiver) = channel();
         AdapterManager {
             peripherals,
-            event_sender: Arc::new(Mutex::new(event_sender)),
-            event_receiver: Arc::new(Mutex::new(Some(event_receiver))),
             async_senders: Arc::new(Mutex::new(vec![])),
         }
     }
@@ -73,23 +51,12 @@ where
             }
             _ => {}
         }
-        // Since we hold a receiver, this will never fail unless we fill the
-        // channel. Whether that's a good idea is another question entirely.
-        self.event_sender
-            .lock()
-            .unwrap()
-            .send(event.clone())
-            .unwrap();
 
         // Remove sender from the list if the other end of the channel has been dropped.
         self.async_senders
             .lock()
             .unwrap()
             .retain(|sender| sender.unbounded_send(event.clone()).is_ok());
-    }
-
-    pub fn event_receiver(&self) -> Option<Receiver<CentralEvent>> {
-        self.event_receiver.lock().unwrap().take()
     }
 
     pub fn event_stream(&self) -> Pin<Box<dyn Stream<Item = CentralEvent>>> {

8ebf2b8d80d1d4287f46297ce2376e586b086d56
Andrew Walbran <qwandor@google.com>
2021-02-15 20:42:13 +0000

    Return error if adapter fails to connect.

--- src/corebluetooth/manager.rs
+++ src/corebluetooth/manager.rs
@@ -17,7 +17,7 @@ impl Manager {
     }
 
     pub async fn adapters(&self) -> Result<Vec<Adapter>> {
-        Ok(vec![Adapter::new()])
+        Ok(vec![Adapter::new().await?])
         // TODO What do we do if there is no bluetooth adapter, like on an older
         // macbook pro? Will BluetoothAdapter::init() fail?
     }

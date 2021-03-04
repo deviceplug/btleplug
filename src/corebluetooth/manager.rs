f59e7062fc3c1606181a997a831e376d008b9a8e
Andrew Walbran <qwandor@google.com>
2021-03-04 12:56:44 +0000

    Add api::Manager trait.

--- src/corebluetooth/manager.rs
+++ src/corebluetooth/manager.rs
@@ -6,7 +6,8 @@
 // for full license information.
 
 use super::adapter::Adapter;
-use crate::Result;
+use crate::{api, Result};
+use async_trait::async_trait;
 
 #[derive(Clone, Debug)]
 pub struct Manager {}
@@ -15,8 +16,13 @@ impl Manager {
     pub async fn new() -> Result<Self> {
         Ok(Self {})
     }
+}
 
-    pub async fn adapters(&self) -> Result<Vec<Adapter>> {
+#[async_trait]
+impl api::Manager for Manager {
+    type Adapter = Adapter;
+
+    async fn adapters(&self) -> Result<Vec<Adapter>> {
         Ok(vec![Adapter::new().await?])
         // TODO What do we do if there is no bluetooth adapter, like on an older
         // macbook pro? Will BluetoothAdapter::init() fail?
--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@
 //! An example of how to use the library to control some BLE smart lights:
 //!
 //! ```rust,no_run
-//! use btleplug::api::{bleuuid::uuid_from_u16, Central, Peripheral as _, WriteType};
+//! use btleplug::api::{bleuuid::uuid_from_u16, Central, Manager as _, Peripheral as _, WriteType};
 //! use btleplug::platform::{Adapter, Manager, Peripheral};
 //! use rand::{Rng, thread_rng};
 //! use std::error::Error;

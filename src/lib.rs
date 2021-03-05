1426a8836b904cdbf75cf2dd723a780bf06b8e69
Andrew Walbran <qwandor@google.com>
2021-03-05 20:59:37 +0000

    CoreBluetooth is for iOS too.

--- src/lib.rs
+++ src/lib.rs
@@ -95,7 +95,7 @@ pub mod winrtble;
 
 #[cfg(target_os = "linux")]
 pub use crate::bluez::{adapter::Adapter, manager::Manager};
-#[cfg(target_os = "macos")]
+#[cfg(any(target_os = "macos", target_os = "ios"))]
 pub use crate::corebluetooth::{adapter::Adapter, manager::Manager};
 #[cfg(target_os = "windows")]
 pub use crate::winrtble::{adapter::Adapter, manager::Manager};

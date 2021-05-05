44081246b03d8ed22d2daa1f2037f06346e2b023
Yuriy Larin <blandger@gmail.com>
2021-05-05 09:31:13 +0300

    added printing platform specific adapter data, fixed compile, updated imports

--- examples/subscribe_notify_characteristic.rs
+++ examples/subscribe_notify_characteristic.rs
@@ -1,7 +1,8 @@
 use std::time::Duration;
 
 use btleplug::api::{ValueNotification, CharPropFlags};
-use btleplug::bluez::{adapter::Adapter, manager::Manager};
+use btleplug::api::{Central, Manager as _, Peripheral};
+use btleplug::platform::{Adapter, Manager};
 use std::io::Cursor;
 use std::error::Error;
 use uuid::Uuid;
@@ -10,6 +11,15 @@ use futures::stream::{StreamExt};
 
 const PERIPHERAL_NAME_MATCH_FILTER: &'static str = "Neuro"; // filter BLE device by partial name
 const NOTIFY_CHARACTERISTIC_UUID:Uuid = Uuid::from_u128(0x6e400002_b534_f393_67a9_e50e24dccA9e); // subscribe UUID
+
+#[cfg(target_os = "linux")]
+fn print_adapter_info(adapter: &Adapter) {
+    println!("connected adapter {:?}", adapter);
+}
+#[cfg(any(target_os = "windows", target_os = "macos"))]
+fn print_adapter_info(_adapter: &Adapter) {
+    println!("adapter info can't be printed on Windows 10 or mac");
+}
 
 /// Processing received BLE data
 fn my_on_notification_handler(data: ValueNotification) {
@@ -32,7 +42,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
         eprint!("Bluetooth adapter(s) were NOT found, sorry...\n");
     } else {
         for adapter in adapter_list.iter() {
-            println!("connecting to BLE adapter {:?}: ...", &adapter);
+            println!("connecting to BLE adapter...");
+            print_adapter_info(&adapter);
 
             adapter
                 .start_scan()
@@ -73,7 +84,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                             );
                             for char_item in chars.into_iter() {
                                 println!("Checking CHARACTERISTIC...: {:?} result = {:?}", char_item.uuid,
-                                         char_item.uuid == subscribe_to_characteristic);
+                                         char_item.uuid == NOTIFY_CHARACTERISTIC_UUID);
                                 // subscribe on selected uuid
                                 if char_item.uuid == NOTIFY_CHARACTERISTIC_UUID
                                     && char_item.properties == CharPropFlags::NOTIFY {

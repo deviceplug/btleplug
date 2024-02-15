0eb6188a6a268dba113bb976d85a0cc28310d6c1
Szymon Lesisz <sz.lesisz@gmail.com>
2024-02-15 17:31:21 +0100

    feat: `winrtble` adapter_info and StateUpdate implementation

--- src/winrtble/adapter.rs
+++ src/winrtble/adapter.rs
@@ -23,19 +23,50 @@ use std::convert::TryInto;
 use std::fmt::{self, Debug, Formatter};
 use std::pin::Pin;
 use std::sync::{Arc, Mutex};
+use windows::{
+    Devices::Radios::{Radio, RadioState},
+    Foundation::TypedEventHandler,
+};
 
 /// Implementation of [api::Central](crate::api::Central).
 #[derive(Clone)]
 pub struct Adapter {
     watcher: Arc<Mutex<BLEWatcher>>,
     manager: Arc<AdapterManager<Peripheral>>,
+    radio: Radio,
+}
+
+// https://github.com/microsoft/windows-rs/blob/master/crates/libs/windows/src/Windows/Devices/Radios/mod.rs
+fn get_central_state(radio: &Radio) -> CentralState {
+    let state = radio.State().unwrap_or(RadioState::Unknown);
+    match state {
+        RadioState::On => CentralState::PoweredOn,
+        RadioState::Off => CentralState::PoweredOff,
+        _ => CentralState::Unknown,
+    }
 }
 
 impl Adapter {
-    pub(crate) fn new() -> Self {
+    pub(crate) fn new(radio: Radio) -> Self {
         let watcher = Arc::new(Mutex::new(BLEWatcher::new()));
         let manager = Arc::new(AdapterManager::default());
-        Adapter { watcher, manager }
+
+        let radio_clone = radio.clone();
+        let manager_clone = manager.clone();
+        let handler = TypedEventHandler::new(move |_sender, _args| {
+            let state = get_central_state(&radio_clone);
+            manager_clone.emit(CentralEvent::StateUpdate(state.into()));
+            Ok(())
+        });
+        if let Err(err) = radio.StateChanged(&handler) {
+            eprintln!("radio.StateChanged error: {}", err);
+        }
+
+        Adapter {
+            watcher,
+            manager,
+            radio,
+        }
     }
 }
 
@@ -102,6 +133,6 @@ impl Central for Adapter {
     }
 
     async fn adapter_state(&self) -> Result<CentralState> {
-        Ok(CentralState::Unknown)
+        Ok(get_central_state(&self.radio))
     }
 }

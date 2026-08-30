5a83163b3c1b80e7629b63e67bd733b41096c26a
Kyle Machulis <kyle@nonpolynomial.com>
2026-08-29 23:37:36 -0700

    test(android): raise instrumentation app minSdk to 24

--- tests/android/build.gradle.kts
+++ tests/android/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.nonpolynomial.btleplug.test"
-        minSdk = 23
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
